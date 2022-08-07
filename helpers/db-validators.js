c9fcaa7658eae8317b99e8443e2955988aea1780
benjaminbascary <benjaminbascary@gmail.com>
2022-08-07 17:22:40 -0300

    Revert "Refactors email validation into helper function"
    This reverts commit 8a5184c4caff1a2b1328a1c0da295a9cecaed499.

--- helpers/db-validators.js
+++ helpers/db-validators.js
@@ -1,23 +1,14 @@
 // Models for validating incoming requests
 const Role = require('../models/role');
-const User = require('../models/user');
 
 const isRoleValid = async (role = '') => {
   // if custom validation does not throw new Error it passes
   const roleExists = await Role.findOne({role: role});
   if (!roleExists) {
-    throw new Error(`The role >${role}< is not registered in DB.`);
-  }
-}
-
-const emailExists = async (email = '') => {
-  const emailExists = await User.findOne({email: email});
-  if (emailExists) {
-    throw new Error(`The email >${email}< is already used.`)
+    throw new Error(`The role >${role}< is not registered in DB`);
   }
 }
 
 module.exports = {
-  isRoleValid,
-  emailExists,
+  isRoleValid
 };
